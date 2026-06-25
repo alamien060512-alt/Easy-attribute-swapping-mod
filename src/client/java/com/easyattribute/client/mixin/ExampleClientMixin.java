eb84908bbe93061dca8bf90c2f1813432d7fb7d0
ImHungry1480 <al.amien060512@gmail.com>
2026-06-25 21:15:17 +0800

    Update ExampleClientMixin.java

--- src/client/java/com/easyattribute/client/mixin/ExampleClientMixin.java
+++ src/client/java/com/easyattribute/client/mixin/ExampleClientMixin.java
@@ -1,18 +1,8 @@
 package com.easyattribute.client.mixin;
 
-import com.easyattribute.client.WeaponSwapKeyHandler;
 import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocalPlayer.class)
 public class ExampleClientMixin {
-    @Inject(method = "swapItemWithOffhand", at = @At("HEAD"), cancellable = true)
-    private void cancelOffhandSwap(CallbackInfo ci) {
-        if (WeaponSwapKeyHandler.swapKey != null && WeaponSwapKeyHandler.swapKey.isDown()) {
-            ci.cancel();
-        }
-    }
 }
