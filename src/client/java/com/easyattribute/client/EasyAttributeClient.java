e7d01de4fef46d4c814a8003ae966144ba8328d3
ImHungry1480 <al.amien060512@gmail.com>
2026-06-25 21:27:09 +0800

    Update EasyAttributeClient.java

--- src/client/java/com/easyattribute/client/EasyAttributeClient.java
+++ src/client/java/com/easyattribute/client/EasyAttributeClient.java
@@ -1,10 +1,19 @@
 package com.easyattribute.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.Minecraft;
 
 public class EasyAttributeClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         WeaponSwapKeyHandler.register();
+
+        // open config screen with config key (default: none, user assigns in controls)
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (WeaponSwapKeyHandler.configKey.consumeClick()) {
+                client.setScreen(new ConfigScreen(client.screen));
+            }
+        });
     }
 }
